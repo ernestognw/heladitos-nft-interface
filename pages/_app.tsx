3112baf64e86df902642a00807d9cf0d0c37ba82
ernestognw <ernestognw@gmail.com>
2022-10-29 15:13:36 -0500

    Added favicon ⭐️

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,24 @@ import "@styles/globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
 import { WagmiConfig } from "wagmi";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chains, wagmiClient } from "@config/wallet";
+import Head from "next/head";
 
 import type { AppProps } from "next/app";
-import { chains, wagmiClient } from "@config/wallet";
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+const HeladitosApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
-        <Component {...pageProps} />
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <>
+      <Head>
+        <title>Heladitos NFT</title>
+      </Head>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains}>
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </>
   );
 };
 
-export default MyApp;
+export default HeladitosApp;
